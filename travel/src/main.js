ddc9f0df1dcc69442b27b9f5406afd88b4199de8
Roger3 <947625259@qq.com>
2019-10-21 14:13:19 +0800

    'swiper'

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App.vue'
 import router from './router'
 import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+require('swiper/dist/css/swiper.css')
 
 Vue.config.productionTip = false;
+
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 new Vue({
   router,
---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,56 @@
+<template>
+        <div class="wrapper">
+            <swiper :options="swiperOption" >
+                <!-- slides -->
+                <swiper-slide v-for="item of swiperList" :key="item.id">
+                    <img class="siwper-img" :src="item.imgUrl" alt="img">
+                </swiper-slide>
+
+                <!-- Optional controls -->
+                <div class="swiper-pagination"  slot="pagination"></div>
+                <!--<div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>-->
+                <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+            </swiper>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data:function () {
+            return{
+                 swiperOption:{
+                     autoplay: 3000,
+                     pagination:'.swiper-pagination',
+                     loop:true
+                 },
+                swiperList:[{
+                     id:'01',
+                    imgUrl:'//mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/abe3418971067bd655fb2794d6e2eeec.jpg'
+                },{
+                     id:'02',
+                    imgUrl:'//mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/b59b8db9f72eb8f54df5bf2155672e9c.jpg'
+                },{
+                    id:'03',
+                    imgUrl:'//mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/3f5ff03fa0c024b930f515e63ae2c702.jpg_945x288_7dff4510.jpg'
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+    .wrapper >>>.swiper-pagination-bullet-active{
+        background: #ffffff;
+    }//>>>可以让这里的样式不受scoped的限制，可以穿透到 .wrapper下面
+    .wrapper{
+        overflow hidden;
+        width 100%;
+        height 0;
+        padding-bottom 31.25%
+    }
+    .siwper-img{
+        width:100%;
+    }
+</style>
