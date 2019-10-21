01389b544a16c21d6a73c4299a16f513f614086b
Roger3 <947625259@qq.com>
2019-10-21 10:41:42 +0800

    '完成首页头部'

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -13,16 +21,21 @@
 </script>
 
 <style scoped lang="stylus">
+    @import '~@/assets/styles/varibles.styl'
     .header{
         display: flex;
         line-height: .86rem;
-        background: #00bcd4;
+        background: $bgColor;
         color: #ffffff;
     }
 
     .header-left{
         width:.64rem;
         float: left;
+    }
+    .header-left .back-icon{
+        text-align: center;
+        font-size: .4rem;
     }
     .header-input{
         flex: 1;
@@ -32,11 +45,16 @@
         border-radius:.1rem;
         margin-top: .12rem;
         margin-left: .12rem;
-        color:#ccc
+        color:#ccc;
+        padding-left: .2rem;
     }
     .header-right{
         width:1.24rem;
         float: right;
         text-align: center;
+    }
+    .header-right .arrow-icon{
+        font-size: .24rem;
+        margin-left: -.04rem;
     }
 </style>
