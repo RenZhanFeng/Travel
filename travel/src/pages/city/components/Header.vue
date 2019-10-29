01b7c38282f32fae9699f766e339ea1641a6e324
Roger3 <947625259@qq.com>
2019-10-29 15:10:10 +0800

    add city router and header

---
+++ travel/src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        <router-link to="/">
+            <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
+        城市选择
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped lang="stylus">
+    @import '~@/assets/styles/varibles.styl'
+.header{
+    position relative;
+    overflow: hidden;
+    height .86rem;
+    line-height .86rem;
+    text-align center;
+    color white;
+    background $bgColor;
+    font-size .32rem;
+}
+    .header-back{
+        position absolute;
+        width .64rem;
+        text-align center;
+        font-size .4rem;
+        color white;
+    }
+</style>
