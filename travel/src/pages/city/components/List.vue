d0350f88e4c6a86122c1078324cc200d7082d8a9
Roger3 <947625259@qq.com>
2019-10-30 14:57:04 +0800

    city-ajax finish

--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -13,96 +13,21 @@
            <div class="area">
                <div class="title">热门城市</div>
                <div class="button-list">
-                   <div class="button-wrapper">
-                       <div class="button">深圳</div>
-                   </div>
-                   <div class="button-wrapper">
-                       <div class="button">深圳</div>
-                   </div>
-                   <div class="button-wrapper">
-                       <div class="button">深圳</div>
-                   </div>
-                   <div class="button-wrapper">
-                       <div class="button">深圳</div>
-                   </div>
-                   <div class="button-wrapper">
-                       <div class="button">深圳</div>
+                   <div class="button-wrapper" v-for="item of hotCities" :key="item.id">
+                       <div class="button">{{item.name}}</div>
                    </div>
 
-
-               </div>
-           </div>
-           <div class="area">
-               <div class="title">A</div>
-               <div class="item-list">
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-
-               </div>
-           </div>
-           <div class="area">
-               <div class="title">A</div>
-               <div class="item-list">
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-
-               </div>
-           </div>
-           <div class="area">
-               <div class="title">A</div>
-               <div class="item-list">
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-
-               </div>
-           </div>
-           <div class="area">
-               <div class="title">A</div>
-               <div class="item-list">
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-
-               </div>
-           </div>
-           <div class="area">
-               <div class="title">A</div>
-               <div class="item-list">
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-                   <div class="item">阿拉尔</div>
-
                </div>
            </div>
 
+           <div class="area" v-for="(item,key) of cities" :key="key">
+               <div class="title">{{key}}</div>
+               <div class="item-list" >
+                   <div class="item"
+                        v-for="innerItem of item"
+                        :key="innerItem.id">{{innerItem.name}}</div>
+               </div>
+           </div>
 
        </div>
     </div>
@@ -112,6 +37,10 @@
     import Bscroll from 'better-scroll'
     export default {
         name: "List",
+        props:{
+            hotCities:Array,
+            cities:Object
+        },
         mounted() {
             this.scroll = new Bscroll(this.$refs.wrapper);
         }
